8f780ec0a4f7ae5750c4e3f2c606ee1b4dbdfc3f
bullockde <bullockde123@gmail.com>
2018-12-07 10:44:59 -0500

    Working App with Dashboard and Views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
+// -- Import Modules -- //
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppRoutingModule } from './app-routing.module';
+// import { HttpClientModule }    from '@angular/common/http';
+// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+
+// -- Import Components -- //
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+// -- Import Services -- //
+
+// import { InMemoryDataService }  from './in-memory-data.service';
+
+
+// -- NgModule Declaration -- //
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+// -- Export Classes -- //
+export class AppModule { 
+
+  
+}
